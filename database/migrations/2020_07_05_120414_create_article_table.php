8c796b928a3318b74053872375c816552a4afdaf
yusrilramadhan23 <67570255+yusrilramadhan23@users.noreply.github.com>
2020-07-05 20:44:51 +0700

    done
    maaf mas kurang maksimal keadaan lagi sakit

---
+++ database/migrations/2020_07_05_120414_create_article_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBiginteger('userid');
+            $table->string('judul');
+            $table->string('tags');
+            $table->string('slug');
+            $table->foreign('userid')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
